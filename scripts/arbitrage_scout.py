2c4a943b8d06c3206e295c292be785312c611aeb
Andrew <andrew@Andrews-MacBook-Pro-2.local>
2022-04-09 17:24:35 -0400

    Andrew Siah ETH Portland Hackathon Submission

--- scripts/arbitrage_scout.py
+++ scripts/arbitrage_scout.py
@@ -1,5 +1,3 @@
-# start --script 2script.py
-
 from decimal import Decimal
 
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
@@ -8,25 +6,36 @@ from hummingbot.core.utils.trading_pair_fetcher import TradingPairFetcher
 s_decimal_0 = Decimal(0)
 
 
-class ArbitrageFinder1(ScriptStrategyBase):
-    # It is best to first use a paper trade exchange connector
-    # while coding your strategy, once you are happy with it
-    # then switch to real one.
+class ArbitrageScout(ScriptStrategyBase):
+    """
+    ArbitrageScout is a meta-strategy that algorithmically searches for arbitrage strategies for you across two exchanges that you insert.
+    Params:
+    Two exchange names
+    min_profitability
+    Algo automatically finds the intersectional sets of tokens that are listed in both markets. Then, every second it iterates through all these markets to find profitable pairs. If profit is more than your min_profitability parameter, it will notify you on telegram.
+    You can then execute a manual arbitrage by buying said token at the cheaper exchange, withdrawing it to the premium exchange, and selling it there.
+    Free money. You're welcome.
+    """
+    # The exchanges you want to scout.
     arbitrage_markets = ["gate_io_paper_trade", "ascend_ex_paper_trade"]
+    # Min profitability before it alerts you.
+    min_profitability = Decimal('0.007')
 
     trading_pair_fetcher: TradingPairFetcher = TradingPairFetcher.get_instance()
     if trading_pair_fetcher.ready:
         trading_pairs_1 = trading_pair_fetcher.trading_pairs.get(arbitrage_markets[0], [])
         trading_pairs_2 = trading_pair_fetcher.trading_pairs.get(arbitrage_markets[1], [])
+
+    # It takes a long time to start up the market pairs if you search through all the markets.
+    # A speedier startup would search through only a splice of the markets.
     trading_pairs_set = list(set(trading_pairs_1).intersection(trading_pairs_2))
-    trading_pairs_set = trading_pairs_set[0:50]
+    trading_pairs_set = trading_pairs_set[:]
 
     markets = {arbitrage_markets[0]: trading_pairs_set, arbitrage_markets[1]: trading_pairs_set}
-    min_profitability = Decimal('0.007')
 
     def on_tick(self):
-        self.logger().info(f"trading_pairs_1 {self.trading_pairs_set}")
-        self.notify_hb_app_with_timestamp(f"1: {self.arbitrage_markets[0]}; 2: {self.arbitrage_markets[1]}")
+        self.logger().info(f"Set of Trading Pairs {self.trading_pairs_set}")
+        self.notify_hb_app_with_timestamp(f"Exchange_1: {self.arbitrage_markets[0]}; Exchange_2: {self.arbitrage_markets[1]}")
         for pair in self.trading_pairs_set:
             try:
                 market_1_bid = self.connectors[self.arbitrage_markets[0]].get_price(pair, False)
@@ -37,8 +46,8 @@ class ArbitrageFinder1(ScriptStrategyBase):
                 profitability_buy_2_sell_1 = market_2_bid / market_1_ask - 1
 
                 if profitability_buy_1_sell_2 > self.min_profitability:
-                    self.notify_hb_app_with_timestamp(f"{pair}: buy_1_sell_2: {profitability_buy_1_sell_2:.5f}")
+                    self.notify_hb_app_with_timestamp(f"{pair}: Buy@1 & Sell@2: {profitability_buy_1_sell_2:.5f}")
                 if profitability_buy_2_sell_1 > self.min_profitability:
-                    self.notify_hb_app_with_timestamp(f"{pair}: buy_2_sell_1: {profitability_buy_2_sell_1:.5f}")
+                    self.notify_hb_app_with_timestamp(f"{pair}: Buy@2 & Sell@1: {profitability_buy_2_sell_1:.5f}")
             except BaseException:
                 self.logger().info(f"{pair} has no bid or ask order book")
